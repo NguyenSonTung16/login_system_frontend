ac1f76f265354e9e4474fdc9eac4d92adb0ac7eb
NguyenSonTung16 <162577055+NguyenSonTung16@users.noreply.github.com>
2025-12-07 20:10:29 +0700

    update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000'; // port Backend
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 // Tạo instance axios chính
 export const api = axios.create({
